4ebc8cde742f870b729902eb68c6d5c450f93701
Richard Oliveira <richard.097@hotmail.com>
2023-06-14 11:59:21 -0300

    Correcoes de using

---
+++ MeditationPt/Infra/MeditationsDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MeditationPt.Infra
+{
+	public class MeditationsDatabaseSettings
+	{
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string MeditationsCollectionName { get; set; } = null!;
+    }
+}
--- MeditationPt/obj/Debug/net7.0/MeditationPt.AssemblyInfo.cs
+++ MeditationPt/obj/Debug/net7.0/MeditationPt.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MeditationEs")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("MeditationPt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("MeditationEs")]
-[assembly: System.Reflection.AssemblyTitleAttribute("MeditationEs")]
+[assembly: System.Reflection.AssemblyProductAttribute("MeditationPt")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MeditationPt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
